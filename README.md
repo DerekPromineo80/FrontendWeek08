1c5e1bac421ef668f45aff495e8e2f747f869cd0
Derek Mcguire <derekrmc@gmail.com>
2025-03-01 21:19:42 -0500

    Week 8 Update

---
+++ README.md
@@ -0,0 +1,5 @@
+Hello!
+
+Welcome to Derek McGuire's Week 8 Coding Assignment!
+
+Enjoy!
---
+++ week08assignment.js
@@ -0,0 +1,144 @@
+/* Week 8 Assignment JavaScript */
+/* By Derek McGuire */
+
+// Defining the Class of Vocabulary Definitions
+class Definition {
+    constructor(definition, part) {
+        this.definition = definition;
+        this.part = part;
+    }
+
+    describe() {
+        return `${this.definition} - Part of Speech: ${this.part}`
+    }
+};
+// End of Vocabulary Definitions class
+
+
+// Defining the Class of Vocabulary Terms
+// Notice how definitions is pluralized
+class Word {
+    constructor(word) {
+        this.word = word;
+        this.definitions = [];
+    }
+
+    addDefinition(definition) {
+        if (definition instanceof Definition) {
+            this.definitions.push(definition);
+        } else {
+            throw new Error(`You can only add a Definition. This is not a definition: ${vocabDefinition}`);
+        }
+    }
+
+    describe() {
+        return `${this.word} has ${this.definitions.length} definitions.`;
+    }
+};
+//
+
+class VocabMenu {
+    constructor() {
+        this.words = [];
+        this.selectedWord = null;
+    }
+
+    start() {
+        let selection = this.showMainMenuOptions();
+        while (selection!= 0) {
+            switch(selection) {
+                case '1' :
+                    this.createWord();
+                    break;
+                case '2' :
+                    this.viewWord();
+                    break;
+                case '3' :
+                    this.deleteWord();
+                    break;
+                case '4' :
+                    this.displayWords();
+                    break;
+                default:
+                    selection = 0;
+            }
+            selection = this.showMainMenuOptions();
+        }
+        alert('Thanks for Reading! Goodbye!');
+    }
+
+    showMainMenuOptions() {
+        return prompt (`
+            0) Exit the Vocabulary Menu
+            1) Create a new Vocabulary Term
+            2) View a Vocabulary Term
+            3) Delete a Vocabulary Term
+            4) Display All Vocabulary Terms
+        `);
+    }
+
+    showWordMenuOptions(wordInfo) {
+        return prompt(`
+            0) Go Back to Vocabulary Menu
+            1) Add a New Defintion
+            2) Delete a Definition
+            - - - - - - - - - 
+            ${wordInfo}
+        `);
+    }
+
+    displayWords() {
+        let wordString = '';
+        for (let i = 0; i < this.words.length; i++) {
+            wordString += i + ') ' + this.words[i].word + '\n';
+        }
+        alert(wordString);
+    }
+
+    createWord() {
+        let word = prompt('Enter a new Vocabulary Term: ');
+        this.words.push(new Word(word));
+    }
+
+    viewWord() {
+        let i = prompt('Enter the index of the Vocabulary Term you want to see:');
+        if (i > -1 && i < this.words.length) {
+            this.selectedWord = this.words[i];
+            let description = 'Vocabulary Term: ' + i + ') ' + this.selectedWord.word + '\n';
+            for (let j = 0; j < this.selectedWord.definitions.length; j++) {
+                description += j + ') ' + this.selectedWord.definitions[j].describe() + '\n';
+            }
+            let selectionOne = this.showWordMenuOptions(description);
+            switch (selectionOne) {
+                case '1' :
+                    this.createDefinition();
+                    break;
+                case '2' :
+                    this.deleteDefinition();
+            }
+        }
+    }
+    
+    deleteWord() {
+        let i = prompt('Enter the index of the definition to delete: ');
+        if (i > -1 && i < this.words.length) {
+            this.words.splice(i,1);
+        }
+    }
+
+    createDefinition() {
+        let definition = prompt('Enter a new definition: ');
+        let part = prompt('Enter a part of speech this defintion fits: ');
+        this.selectedWord.addDefinition(new Definition(definition,part));
+    }
+
+    deleteDefinition() {
+        let q = prompt('Enter the index of the definition that you wish to delete: ');
+        if (q > -1 && q < this.selectedWord.definitions.length) {
+            this.selectedWord.definitions.splice(q,1);
+        }
+    }
+};
+
+let vocabMenu = new VocabMenu();
+vocabMenu.start();
